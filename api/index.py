d2e33113219aab8ac6ef98fcd6046edd2e378f4c
hub-for-aftertechnologies <aftertechnologies@hotmail.com>
2024-09-07 12:03:45 +0300

    Adding sarufi module

--- api/index.py
+++ api/index.py
@@ -1,3 +1,11 @@
 from sarufi.middleware import create_app
+from flask import jsonify
 
 app = create_app()
+
+@app.route('/')
+def sarufi_middleware_index_route():
+    return jsonify({'message': 'Welcome to Sarufi middleware!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- api/sarufi/middleware.py
+++ api/sarufi/middleware.py
@@ -4,8 +4,4 @@ def create_app():
     
     app = Flask(__name__)
     
-    @app.route('/')
-    def sarufi_middleware_index_route():
-        return jsonify({'message': 'Welcome to Sarufi middleware!'})
-    
     return app
