ca9d4664697a117139e424668da6ece08956d675
hub-for-aftertechnologies <aftertechnologies@hotmail.com>
2024-09-07 15:41:02 +0300

    add

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 #####################################################################################################
 # Adding Blueprints from blueprints
 #####################################################################################################
-from api.sarufi.middleware import sarufi_middleware
+from sarufi.middleware import sarufi_middleware
 
 # Initialize Flask app
 app = Flask(__name__)
--- api/sarufi/middleware.py
+++ api/sarufi/middleware.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from api.sarufi.zenopay import make_payment
+from sarufi.zenopay import make_payment
 
 name = "sarufi_middleware"
 sarufi_middleware = Blueprint(name, __name__)
@@ -9,6 +9,7 @@ sarufi_middleware = Blueprint(name, __name__)
 def sarufi_payment_middleware():
     # Get the request data
     data = request.get_json()
+    print(data)
 
     # making actuall payment with zenopay
     make_payment(
